1da5dc1a44641586820bc31a93f7992c7c1cbf7e
Oriam De Gyves <oriamdl@gmail.com>
2017-08-21 19:33:41 -0500

    Clase 21/08/2017

--- 201713-TC3022-2/Main.cpp
+++ 201713-TC3022-2/Main.cpp
@@ -1,22 +1,65 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <vector>
+
+// Identificador del manager al que vamos a asociar todos los VBOs
+GLuint vao;
+
+void Initialize()
+{
+	// Creando toda la memoria que el programa va a utilizar.
+	
+	// Creación del atributo de posiciones de los vértices.
+	// Lista de vec2
+	// Claramente en el CPU y RAM
+	std::vector<glm::vec2> positions;
+	positions.push_back(glm::vec2(-1.0f, -1.0f));
+	positions.push_back(glm::vec2(1.0f, -1.0f));
+	positions.push_back(glm::vec2(0.0f, 1.0f));
+
+	// Queremos generar 1 manager
+	glGenVertexArrays(1, &vao);
+	// Utilizar el vao. A partir de este momento, todos VBOs creados y configurados
+	// se van a asociar a este manager.
+	glBindVertexArray(vao);
+
+	// Identificador del VBO de posiciones.
+	GLuint positionsVBO;
+	// Creación del VBO de posiciones
+	glGenBuffers(1, &positionsVBO);
+	// Activamos el buffer de posiciones para poder utilizarlo
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
+	// Creamos la memoria y la inicializamos con los datos del atributo de posiciones
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * positions.size(), 
+		positions.data(), GL_STATIC_DRAW);
+	// Activamos el atributo en la tarjeta de video
+	glEnableVertexAttribArray(0);
+	// Configuramos los datos del atributo en la tarjeta de video
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+	// Ya no vamos a utilizar este VBO en este momento.
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desactivamos el manager
+	glBindVertexArray(0);
+}
 
 void GameLoop()
 {
+	// Limpiamos el buffer de color y el buffer de profunidad.
+	// Siempre hacerlo al inicio del frame
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// ¡¡¡¡¡WARNING!!!!! Esto es OpenGL viejito. Sólamente lo vamos a usar esta clase.
-	// Prohibido el resto del semestre.
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(-1.0f, -1.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex2f(1.0f, -1.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex2f(0.0f, 1.0f);
-	glEnd();
-	// ¡¡¡¡¡END WARNING!!!!!
+	// Activamos el manager, en este momento se activan todos los
+	// VBOs asociados automáticamente.
+	glBindVertexArray(vao);
+	// Función de dibujado sin indices.
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	// Terminamos de utilizar el manager
+	glBindVertexArray(0);
 
+	// Cuando terminamos de renderear, cambiamos los buffers.
 	glutSwapBuffers();
 }
 
@@ -26,11 +69,13 @@ int main(int argc, char* argv[])
 	// Freeglut se encarga de crear una ventana
 	// en donde podemos dibujar
 	glutInit(&argc, argv);
+	// Solicitando una versión específica de OpenGL.
+	glutInitContextVersion(4, 4);
 	// Iniciar el contexto de OpenGL. El contexto se refiere
 	// a las capacidades que va a tener nuestra aplicación
 	// gráfica.
-	// En este caso estamos trabajando con el pipeline clásico.
-	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+	// En este caso estamos trabajando con el pipeline programable.
+	glutInitContextProfile(GLUT_CORE_PROFILE);
 	// Freeglut nos permite configurar eventos que ocurren en la ventana.
 	// Un evento que nos interesa es cuando alguien cierra la ventana.
 	// En este caso, simplemente dejamos de renderear la esscena y terminamos el programa.
@@ -52,6 +97,11 @@ int main(int argc, char* argv[])
 	// Configurar OpenGL. Este es el color por default del buffer de color
 	// en el framebuffer.
 	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	// Configuración inicial de nuestro programa.
+	Initialize();
 
 	// Iniciar la aplicación. El main se pausará en esta línea hasta que se cierre
 	// la venta de OpenGL.
